7829012c5a43a76f1e136130ed13da6d95cf8e01
Maryust <okmijn0001@ya.ru>
2022-01-25 19:31:52 +0300

    Fail test added

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
 // Проверки
         .then()
         .statusCode(200)
-        .body("data", equalTo("Everybody - dance"))
+        .body("data", equalTo("Everybody"))
         ;
     }
 
